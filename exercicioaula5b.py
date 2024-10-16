4226221001ec625431aa82aa5719dcc513377efb
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-16 20:31:58 -0300

    Add files via upload

---
+++ exercicioaula5b.py
@@ -0,0 +1,7 @@
+# Comparação de números
+a = float(input('Informe o primeiro número \n'))
+b = float(input('Informe o segundo número \n'))
+if a == b:
+ print('Os números são iguais ')
+else:
+ print('São diferentes ') 
